57e3eb46c3a3a27002cce6352d3d9e534cb3cb11
adwitz <adam.witzel@gmail.com>
2014-10-25 14:05:42 -0700

    Parser parses data, creates object and gets written to text file

--- data/parser.js
+++ data/parser.js
@@ -9,8 +9,63 @@ var cleanUp = function(item){
   return true;
 };
 
-var sets = [[1,2,2], [2,2,1], [1,2,2], [2,2,1], [1,2,1], [2,2,1], [2,2,1],
-            [2,2,1], [2,2,1], [2,2,1], [2,2,1], [2,2,1], [1,2,2], [1,1,1]];
+var setType = function(rep){
+
+  if (rep === 'Neg'){
+    return 'negative';
+  } else if (rep === 'F'){
+    return 'failure';
+  } else {
+    return 'normal';
+  }
+
+};
+
+var processReps = function(reps){
+  var processed = reps.map(function(item){
+    if (item === 'Neg' || item === 'F'){
+      return 1;
+    } else {
+      return Number(item);
+    }
+  });
+  return processed;
+};
+
+var sets = [1,2,2,2,2,1,1,2,2,2,2,1,1,2,1,2,2,1,2,2,1,2,2,1,2,2,1,2,2,1,2,2,1,2,2,1,1,2,2,1,1,1];
+
+var populateSets = function(startIndex, regimen, reps){
+  var workout = [], pos;
+  var type;
+  
+  for (var i=0; i<3; i++){
+    for (var j=0; j<sets[startIndex + i]; j++){
+
+      type = setType(reps.raw[startIndex + i]);
+
+      workout.push({
+        reps: reps.processed[startIndex + i],
+        weight: regimen[startIndex + i],
+        completed: false,
+        type: type
+      }); //individual workout goes here      
+    }
+
+  }
+
+  return workout;
+};
+
+createRepObj = function(repsRow){
+
+  var raw = repsRow;
+  var processed = processReps(raw);
+
+  return {
+    raw: raw,
+    processed: processed
+  };
+};
 
 fs.readFile('data/bench-data.csv', 'utf-8', function(err, data){
   if (err) throw err;
@@ -19,7 +74,7 @@ fs.readFile('data/bench-data.csv', 'utf-8', function(err, data){
   var result = [];
   var rows = data.trim().replace(re, '').split('\n');
 
-  var processed = [];
+  var processed = {};
 
   processed = rows.map(function(row, index, array){
 
@@ -32,12 +87,35 @@ fs.readFile('data/bench-data.csv', 'utf-8', function(err, data){
     return row;
 
   });
-  
-  var reps = processed[2].slice(1);
 
-  return result;
+  var reps = createRepObj(processed[2].slice(1));
+
+  var oneRMs = processed.slice(4);
+
+  var oneRM, sets;
+  
+  var allWorkoutData = {};
+
+  oneRMs.forEach(function(regimen, oneRMIndex, body){
+    oneRM = regimen[0];
+    allWorkoutData[oneRM] = [];
+    for (var i=1; i<regimen.length; i+=3){
+      sets = populateSets(i, regimen, reps);
+      allWorkoutData[oneRM].push({
+        id: Math.floor(i / 3),
+        workout: Math.floor(i / 3) + 1,
+        sets: sets
+      });
+    }
+    
+  });
+
+  fs.writeFile("data/processed.txt", JSON.stringify(allWorkoutData), function(err){
+    if (err){
+      console.log(err);
+    } else {
+      console.log('wrote data to processed.txt');
+    }
+  });
 
 });
-
-
-//{reps: 5, weight: 100, completed: false, type: 'normal'},
