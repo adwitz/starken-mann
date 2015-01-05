916bf96e0e42b92726a3c0b99d0243ce52ce42a0
A Witzel <adam.witzel@digitalinsight.com>
2015-01-05 09:23:43 -0800

    Add one rep max to settings view

--- www/js/controllers/settingsCtrl.js
+++ www/js/controllers/settingsCtrl.js
@@ -2,4 +2,14 @@ angular.module('bench.controllers')
 
 .controller('SettingsCtrl', function($scope, $localstorage) {
 
+  var getOneRepMaxValue = function(){
+    var oneRepMax = JSON.parse($localstorage.get('max'));
+    if (oneRepMax) {
+      return oneRepMax;
+    }
+    return undefined;
+  };
+
+  $scope.oneRepMax = getOneRepMaxValue();
+
 });
